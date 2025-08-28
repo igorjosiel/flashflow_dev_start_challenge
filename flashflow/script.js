6030689029ac4df6477a8ffed6a17d9856c28830
Igor Josiel <igorjosielvitalino@gmail.com>
2025-08-28 14:23:39 -0300

    feat: prepara o método para iniciar o jogo.

--- flashflow/script.js
+++ flashflow/script.js
@@ -1,1 +1,31 @@
-console.log("Teste");
+const flashcards = [
+	{
+		question: "O que é JavaScript?",
+		answer: "JavaScript é uma linguagem de programação interpretada e orientada a objetos, usada principalmente para adicionar interatividade e dinamismo a páginas web."
+	},
+	{
+		question: "O que são funções de callback em JavaScript?",
+		answer: "São funções passadas como argumento para outra função, que serão executadas após um determinado evento ou operação."
+	},
+	{
+		question: "O que é o this em JavaScript?",
+		answer: "this se refere ao contexto de execução atual. Seu valor varia dependendo de como a função é chamada."
+	},
+	{
+		question: "O que é hoisting em JavaScript?",
+		answer: 'Hoisting é o comportamento em que declarações de variáveis e funções são "movidas" para o topo do escopo durante a fase de compilação. Isso faz com que seja possível usar uma função antes de sua definição no código.'
+	},
+	{
+		question: "O que são tipos primitivos em JavaScript?",
+		answer: "Os tipos primitivos em JavaScript são: String, Number, Boolean, Null, Undefined, BigInt e Symbol. Eles representam valores imutáveis e não são objetos."
+	},
+];
+
+
+const startButton = document.querySelector("button.button-start");
+
+function startFlashCards() {
+    console.log("Teste");
+}
+
+startButton.addEventListener("click", startFlashCards);
